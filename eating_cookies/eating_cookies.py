f8608f2a5dd61017755074d37959b1c010e15af1
GeraldRyan <gerald.ryan40@yahoo.com>
2020-06-17 18:44:05 -0500

    didnt get it today

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,18 +5,33 @@ Returns: an integer
 def eating_cookies(n):
     # Your code here
     r = n 
-    large_bites, medium_bites, small_bites, ways = 0,0,0, 0
-    while r >= 3:
-        r = r-3
-        large_bites += 1
-    while r >=2:
-        r = r-2
-        medium_bites += 1
-    while r>=1:
-        r = r-1
-        small_bites +=1
+    if n ==1:
+        return 1
+    if n == 0:
+        return 1
+    if n ==2:
+        return 2
+    max_large_bites, ways = n//3,0
+    # while r >= 3:
+    #     r = r-3
+    #     large_bites += 1
+    # while r >=2:
+    #     r = r-2
+    #     medium_bites += 1
+    # while r>=1:
+    #     r = r-1
+    #     small_bites +=1
 
-    print("large, medium and small bites", large_bites, medium_bites, small_bites)
+    for large_bites in range(max_large_bites,0,-1):
+        max_medium_bites = n-(large_bites*3)//2
+        for medium_bites in range(max_medium_bites,0,-1):
+            ways +=1
+    # if (max_large_bites == 0) and (n//2 ==0):
+    #     ways = 1
+
+    return ways
+
+    # print("large, medium and small bites", large_bites, medium_bites, small_bites)
     
 
 if __name__ == "__main__":
