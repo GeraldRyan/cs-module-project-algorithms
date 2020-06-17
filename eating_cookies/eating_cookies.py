8bfc232b232a7ecc9f2c85fa175813fe62faa629
GeraldRyan <gerald.ryan40@yahoo.com>
2020-06-17 17:26:04 -0500

    stuck on cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -4,8 +4,20 @@ Returns: an integer
 '''
 def eating_cookies(n):
     # Your code here
+    r = n 
+    large_bites, medium_bites, small_bites, ways = 0,0,0, 0
+    while r >= 3:
+        r = r-3
+        large_bites += 1
+    while r >=2:
+        r = r-2
+        medium_bites += 1
+    while r>=1:
+        r = r-1
+        small_bites +=1
 
-    pass
+    print("large, medium and small bites", large_bites, medium_bites, small_bites)
+    
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
